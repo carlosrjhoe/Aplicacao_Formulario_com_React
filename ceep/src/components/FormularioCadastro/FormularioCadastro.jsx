bc68401598b159b753f4a2a59018fdbd163e9cbb
Carlos Roberto <carlosrjhoe@gmail.com>
2022-04-13 22:59:51 -0300

    Rever a aula (separando formulário) para encontrar o bug

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuarios from "./DadosUsuarios";
+import DadosEntrega from "./DadosEntrega";
+import { Typography } from "@material-ui/core";
 
 function FormularioCadastro({ aoEnviar, validaCPF }) {
+  const [etapaAtual, setEtapaAtual] = useState(1);
+
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuarios />;
+      case 1:
+        return <DadosPessoais aoEnviar={aoEnviar} validaCPF={validaCPF}/>;
+      case 2:
+        return <DadosEntrega />;
+      default:
+        return <Typography>Erro</Typography>
+    }
+  }
   return (
     <>
-      <DadosPessoais aoEnviar={aoEnviar} validaCPF={validaCPF} />
-      <DadosUsuarios />
+      {formularioAtual(etapaAtual)}
     </>
   );
 }
+
+
 
 export default FormularioCadastro;
